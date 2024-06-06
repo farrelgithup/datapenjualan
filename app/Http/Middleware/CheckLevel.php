1ee33bd87a9a43cb9df17ae1f87a2d170e95e3bf
pauwuwel <pauuwell999@gmail.com>
2024-06-06 09:38:17 +0900

    upload

---
+++ app/Http/Middleware/CheckLevel.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+use Illuminate\Support\Facades\Auth;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckLevel
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, ...$roles): Response
+    {
+        if (Auth::user()->level === 'admin') {
+            return $next($request);
+        } else {
+            if (in_array(Auth::user()->level, $roles)) {
+                return $next($request);
+            } else {
+                return redirect()->to('/login', 302);
+            }
+        }
+    }
+}
